a1e1d89ba5677393e9fc7e4a916ff0e051aa5ccd
Nomis3167 <113910266+simon3176@users.noreply.github.com>
2022-11-21 09:54:25 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    sprite += 1
+    basic.showNumber(Num)
+})
+function MoveNum (sprite: game.LedSprite, num: number) {
+	
+}
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    sprite += -1
+    basic.showNumber(Num)
+})
+let sprite = 0
+let Num = 0
+Num = randint(0, 20)
+basic.forever(function () {
+	
+})
